0b946e4f23d39d5aa4b1ef8126dff718b00e9b90
Felipe Gasparini <felipe.gasparini@olx.com>
2018-06-15 11:56:37 +0200

    new user

--- CTF_Lab/src/main/resources/data.sql
+++ CTF_Lab/src/main/resources/data.sql
@@ -9,3 +9,6 @@ INSERT INTO concerts (band, concert_date, description, image_url) VALUES ('The P
 
 INSERT INTO users (username, email, password, role) VALUES ('admin', 'admin_ticket@mailinator.com', '$2a$10$zpnfXD7Av9YUhG3obNrtI.QHTDXEhqes.PHlavIsoiUjnswGgrxwS', 'ADMIN');
 
+
+INSERT INTO users (username, email, password, role) VALUES ('mark', 'mark@mailinator.com', '$2a$10$QxtktyF086GMuYLYIraGgOGOI.EZtPLI.eG75D7EiZjcn2u.GUWnu', 'ADMIN');
+
