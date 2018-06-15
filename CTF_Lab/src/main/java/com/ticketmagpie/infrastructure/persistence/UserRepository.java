836b340fbb797fd622d32f25c52bd4523900b497
Felipe Gasparini <felipe.gasparini@olx.com>
2018-06-15 10:54:44 +0200

    sql fix

--- CTF_Lab/src/main/java/com/ticketmagpie/infrastructure/persistence/UserRepository.java
+++ CTF_Lab/src/main/java/com/ticketmagpie/infrastructure/persistence/UserRepository.java
@@ -15,38 +15,35 @@ import com.ticketmagpie.User;
 
 @Component
 public class UserRepository {
-  @Autowired
-  private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-  public User get(String username) {
-    try {
-      return jdbcTemplate.queryForObject("SELECT * FROM users WHERE username=?", (rs, rowNum) -> toUser(rs), username);
-    } catch (EmptyResultDataAccessException e) {
-      return null;
+    public User get(String username) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM users WHERE username=?", (rs, rowNum) -> toUser(rs), username);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
-  }
 
-  public User checkUsernameAndPassword(String username, String password) {
-    return jdbcTemplate.queryForObject(passwordCheckQuery(username, password), (rs, rowNum) -> toUser(rs));
-  }
+    public User checkUsernameAndPassword(String username, String password) {
+        String query = "SELECT * FROM users WHERE username=? AND password=?";
+        return jdbcTemplate.queryForObject(query, (rs, rowNum) -> toUser(rs), username, password);
+    }
 
-  public List<User> getAllUsers() {
-    return jdbcTemplate.query("SELECT * FROM users", (rs, rowNum) -> toUser(rs));
-  }
+    public List<User> getAllUsers() {
+        return jdbcTemplate.query("SELECT * FROM users", (rs, rowNum) -> toUser(rs));
+    }
 
-  public void delete(String username) {
-    jdbcTemplate.update("DELETE FROM users WHERE username=?", username);
-  }
+    public void delete(String username) {
+        jdbcTemplate.update("DELETE FROM users WHERE username=?", username);
+    }
 
-  public void save(User user) {
-    jdbcTemplate.update("INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)", user.getUsername(), user.getPassword(), user.getEmail(), user.getRole());
-  }
+    public void save(User user) {
+        jdbcTemplate.update("INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)", user.getUsername(), user.getPassword(), user.getEmail(), user.getRole());
+    }
 
-  private String passwordCheckQuery(String username, String password) {
-    return format("SELECT * FROM users WHERE username='%s' AND password='%s'", username, password);
-  }
-
-  private User toUser(ResultSet rs) throws SQLException {
-    return new User(rs.getString("username"), rs.getString("password"), rs.getString("email"), rs.getString("role"));
-  }
+    private User toUser(ResultSet rs) throws SQLException {
+        return new User(rs.getString("username"), rs.getString("password"), rs.getString("email"), rs.getString("role"));
+    }
 }
